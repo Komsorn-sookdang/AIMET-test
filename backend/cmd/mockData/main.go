a49c08f11344c4f8cb2c846792c03ad680ce5771
มึน <komsornzaza@gmail.com>
2023-05-02 23:28:06 +0700

    Change auto generate mock data algo

--- backend/cmd/mockData/main.go
+++ backend/cmd/mockData/main.go
@@ -24,22 +24,22 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	events := make([]interface{}, 100000)
-	minDate := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
-	maxDate := time.Date(3000, 1, 1, 0, 0, 0, 0, time.UTC)
-	duration := maxDate.Sub(minDate)
 	for i := 0; i < 100000; i++ {
+		// generate random start time
 		startHour := rand.Intn(24)
 		startMinute := rand.Intn(60)
 
+		// generate random end time
 		endHour := startHour + rand.Intn(24-startHour)
 		endMinute := rand.Intn(60)
 
+		// format time
 		startTime := fmt.Sprintf("%02d:%02d", startHour, startMinute)
 		endTime := fmt.Sprintf("%02d:%02d", endHour, endMinute)
 
 		events[i] = models.Event{
 			Name:      "Event " + strconv.Itoa(i),
-			Date:      minDate.Add(time.Duration(rand.Int63n(int64(duration)))),
+			Date:      randomDate(),
 			StartTime: startTime,
 			EndTime:   endTime,
 		}
@@ -53,4 +53,22 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Complete insert event data (~100,000 records)")
+}
+
+func randomDate() time.Time {
+	// Set the range of years.
+	minYear := 2000
+	maxYear := 2999
+
+	// Generate a random year.
+	year := rand.Intn(maxYear-minYear+1) + minYear
+
+	// Generate a random day of the year.
+	daysInYear := time.Date(year, 12, 31, 0, 0, 0, 0, time.UTC).YearDay()
+	dayOfYear := rand.Intn(daysInYear) + 1
+
+	// Create a time.Time object for the random date.
+	date := time.Date(year, 1, 1, 0, 0, 0, 0, time.UTC).AddDate(0, 0, dayOfYear-1)
+
+	return date
 }
