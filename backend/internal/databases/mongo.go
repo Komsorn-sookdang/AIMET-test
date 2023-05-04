077790d6ac81ff3966aa4be5fa0b8b482f8b9e31
มึน <komsornzaza@gmail.com>
2023-05-05 02:02:57 +0700

    Make Backend restart alway

--- backend/internal/databases/mongo.go
+++ backend/internal/databases/mongo.go
@@ -22,7 +22,7 @@ func getMongoURI() (string, error) {
 }
 
 func InitMongoClient() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	mongoURI, err := getMongoURI()
