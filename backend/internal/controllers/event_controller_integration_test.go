22c098ed5112bae3935f01b1736fc5a7830b25d1
มึน <komsornzaza@gmail.com>
2023-05-05 01:48:58 +0700

    Make integration test more covered

--- backend/internal/controllers/event_controller_integration_test.go
+++ backend/internal/controllers/event_controller_integration_test.go
@@ -8,6 +8,7 @@ import (
 	"aimet-test/internal/repositories"
 	"aimet-test/internal/usecases"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -78,5 +79,100 @@ func TestGetFilteredEventsIntegrationUsecase(t *testing.T) {
 			expectedJSON, _ := json.Marshal(expected)
 			assert.Equal(t, expectedJSON, w.Body.Bytes())
 		}
+	})
+
+	t.Run("should return 400 Bad Request when month is not provided", func(t *testing.T) {
+
+		eventRepo := repositories.NewEventRepositoryMock()
+		eventUsc := usecases.NewEventUsecase(eventRepo)
+		eventCtl := controllers.NewEventController(eventUsc)
+
+		gin.SetMode(gin.TestMode)
+		app := gin.New()
+		app.GET("/events", eventCtl.GetFilteredEvents)
+
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/events?year=2021&day=10"), nil)
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("should return 400 Bad Request when year is invalid", func(t *testing.T) {
+
+		year := -1
+
+		eventRepo := repositories.NewEventRepositoryMock()
+		eventUsc := usecases.NewEventUsecase(eventRepo)
+		eventCtl := controllers.NewEventController(eventUsc)
+
+		gin.SetMode(gin.TestMode)
+		app := gin.New()
+		app.GET("/events", eventCtl.GetFilteredEvents)
+
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/events?month=1&year=%v&day=10", year), nil)
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("should return 400 Bad Request when date is invalid", func(t *testing.T) {
+
+		day := 29
+		year := 2023
+		month := 2
+
+		eventRepo := repositories.NewEventRepositoryMock()
+		eventUsc := usecases.NewEventUsecase(eventRepo)
+		eventCtl := controllers.NewEventController(eventUsc)
+
+		gin.SetMode(gin.TestMode)
+		app := gin.New()
+		app.GET("/events", eventCtl.GetFilteredEvents)
+
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/events?month=%v&year=%v&day=%v", month, year, day), nil)
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("should return 400 Bad Request when sort_by is invalid", func(t *testing.T) {
+
+		sort_by := "invalid"
+
+		eventRepo := repositories.NewEventRepositoryMock()
+		eventUsc := usecases.NewEventUsecase(eventRepo)
+		eventCtl := controllers.NewEventController(eventUsc)
+
+		gin.SetMode(gin.TestMode)
+		app := gin.New()
+		app.GET("/events", eventCtl.GetFilteredEvents)
+
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/events?month=1&year=2021&day=10&sort_by=%v", sort_by), nil)
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("should return 500 Internal Server Error when repository returns error", func(t *testing.T) {
+		
+		eventRepo := repositories.NewEventRepositoryMock()
+
+		eventRepo.On("FindByMonth", 1, 2021).Return([]*models.Event{}, errors.New("error"))
+		eventUsc := usecases.NewEventUsecase(eventRepo)
+		eventCtl := controllers.NewEventController(eventUsc)
+
+		gin.SetMode(gin.TestMode)
+		app := gin.New()
+		app.GET("/events", eventCtl.GetFilteredEvents)
+
+		req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/events?month=1&year=2021&day=10"), nil)
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	})
 }
